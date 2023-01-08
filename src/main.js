b21476649c58523f31fc203980d0f9b866a04a57
CodeGetters <109447998+CodeGetters@users.noreply.github.com>
2023-01-08 22:50:14 +0800

    补充输入框图标

--- src/main.js
+++ src/main.js
@@ -7,14 +7,17 @@ import store from '@/store'
 // Element-Plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // 创建app实例
 const app = createApp(App)
 
 // 挂载
-app.use(ElementPlus)
 app.use(store)
 app.use(router)
-
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 // 挂载实例
 app.mount('#app')
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,12 +1,12 @@
 <template>
   <el-row style="margin: 0" class="container">
-    <el-col :md="12" :lg="12"  class="left"
+    <el-col :md="12" :lg="12" class="left"
     >
       <div class="leftCon">
         <video src="../assets/video/login.mp4" autoplay loop muted></video>
       </div>
     </el-col>
-    <el-col :md="12" :lg="12"  class="right"
+    <el-col :md="12" :lg="12" class="right"
     >
       <h2>欢迎回来 程序猿</h2>
       <div class="greetText">
@@ -23,14 +23,27 @@
           class="demo-ruleForm"
       >
         <el-form-item label="用户名" prop="username">
-          <el-input v-model="ruleForm.username" autocomplete="off"/>
+          <el-input v-model="ruleForm.username" autocomplete="off">
+            <template #prefix>
+              <el-icon>
+                <User/>
+              </el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
           <el-input
               v-model="ruleForm.password"
               type="password"
               autocomplete="off"
-          />
+              show-password
+          >
+            <template #prefix>
+              <el-icon>
+                <Lock/>
+              </el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm(ruleFormRef)"
@@ -44,54 +57,6 @@
 </template>
 
 <script setup lang="ts">
-import {reactive, ref} from 'vue'
-import type {FormInstance} from 'element-plus'
-
-const ruleFormRef = ref<FormInstance>()
-
-
-const validatePass = (rule: any, value: any, callback: any) => {
-  if (value === '') {
-    callback(new Error('Please input the password'))
-  } else {
-    if (ruleForm.password !== '') {
-      if (!ruleFormRef.value) return
-      ruleFormRef.value.validateField('checkPass', () => null)
-    }
-    callback()
-  }
-}
-const validatePass2 = (rule: any, value: any, callback: any) => {
-  if (value === '') {
-    callback(new Error('Please input the password again'))
-  } else if (value !== ruleForm.username) {
-    callback(new Error("Two inputs don't match!"))
-  } else {
-    callback()
-  }
-}
-
-const ruleForm = reactive({
-  username: '',
-  password: '',
-})
-
-const rules = reactive({
-  pass: [{validator: validatePass, trigger: 'blur'}],
-  checkPass: [{validator: validatePass2, trigger: 'blur'}],
-})
-
-const submitForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.validate((valid) => {
-    if (valid) {
-      console.log('submit!')
-    } else {
-      console.log('error submit!')
-      return false
-    }
-  })
-}
 
 </script>
 
